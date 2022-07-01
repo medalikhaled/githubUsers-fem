2db23d7e242a9f8d6fa6be84bbb310cf5682067e
Med_Ali_Khaled <72525144+MEDALIALPHA331@users.noreply.github.com>
2022-07-01 18:22:51 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css';
 import Results from './components/Results';
 import Header from './components/Header';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [bkg, setbkg] = useState('white');
+
+  useMemo(() => {
+    const background = document.body.style;
+    background.backgroundColor === 'black' || background.backgroundColor === 'brown'
+      ? (background.color = 'white')
+      : (background.color = 'black');
+
+    return (background.backgroundColor = bkg);
+  }, [bkg]);
+
   return (
     <div className='App'>
-      <Header />
+      <Header bkg={bkg} setbkg={setbkg} />
       <Results />
     </div>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,32 @@
-/*
-import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-*/
-
+/* eslint-disable react/prop-types */
 import './index.css';
-const Header = () => {
-  return <header>hello</header>;
+const Header = ({ bkg, setbkg }) => {
+  return (
+    <div className='header'>
+      <label>
+        Background Color:{' '}
+        <select
+          value={bkg}
+          onChange={(event) => {
+            event.preventDefault();
+            setbkg(event.target.value);
+          }}
+          onBlur={(event) => {
+            event.preventDefault();
+            event.target.value = bkg;
+          }}
+        >
+          <option value='white'>white</option>
+          <option value='black'>black</option>
+          <option value={'lightblue'}>lightblue</option>
+          <option value={'yellow'}>Yellow</option>
+          <option value={'orange'}>Orange</option>
+          <option value={'pink'}>Pink</option>
+          <option value={'brown'}>Brown </option>
+        </select>
+      </label>
+    </div>
+  );
 };
 
 export default Header;
